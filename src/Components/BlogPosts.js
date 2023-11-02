847af24b956edff2dc749621fe34dbe2f0c394a2
Richard Hallam <richardhallam25@gmail.com>
2023-11-02 13:20:51 +0000

    fixed blogPost return

--- src/Components/BlogPosts.js
+++ src/Components/BlogPosts.js
@@ -3,12 +3,11 @@ import '../styles/BlogPosts.css';
 
 class BlogPosts extends React.Component{
     render() {
-        return 
-            <div>
-                <ul className="post-list">
-                    <li className="post">test</li>
-                </ul>
-            </div>
+        return <div>
+            <ul className="post-list">
+              <li className="post">test</li>
+            </ul>
+        </div>
     }
 }
 
