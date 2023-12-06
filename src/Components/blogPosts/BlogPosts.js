a02087ff442fef30a2c470990c37e390b3f29b5c
Richard Hallam <62851758+Richard-Hallam@users.noreply.github.com>
2023-12-06 19:06:56 +0000

    setup blog post summary flexbox

--- src/Components/blogPosts/BlogPosts.js
+++ src/Components/blogPosts/BlogPosts.js
@@ -6,8 +6,26 @@ function BlogPosts(){
 
         return <div>
             <ul class="p-10">
-              <li class="flex ">
-                <div class="max-w-sm rounded overflow-hidden shadow-lg bg-slate-400">
+              <li class="flex gap-4 flex-col items-center">
+                <div class="max-w-sm rounded overflow-hidden shadow-lg bg-slate-400 max-w-screen-lg">
+                    <h3 class="text-2xl font-bold font-mono text-center">Title</h3>
+                    <img src={require('../../Images/cameraPicture.jpg')} alt='me with a camera'></img>
+                    <p class="text-base font-bold">subheading</p>
+                    <p class="text-base font-mono">body text</p>
+                </div>
+                <div class="max-w-sm rounded overflow-hidden shadow-lg bg-slate-400 max-w-screen-lg">
+                    <h3 class="text-2xl font-bold font-mono text-center">Title</h3>
+                    <img src={require('../../Images/cameraPicture.jpg')} alt='me with a camera'></img>
+                    <p class="text-base font-bold">subheading</p>
+                    <p class="text-base font-mono">body text</p>
+                </div>
+                <div class="max-w-sm rounded overflow-hidden shadow-lg bg-slate-400 max-w-screen-lg">
+                    <h3 class="text-2xl font-bold font-mono text-center">Title</h3>
+                    <img src={require('../../Images/cameraPicture.jpg')} alt='me with a camera'></img>
+                    <p class="text-base font-bold">subheading</p>
+                    <p class="text-base font-mono">body text</p>
+                </div>
+                <div class="max-w-sm rounded overflow-hidden shadow-lg bg-slate-400 max-w-screen-lg">
                     <h3 class="text-2xl font-bold font-mono text-center">Title</h3>
                     <img src={require('../../Images/cameraPicture.jpg')} alt='me with a camera'></img>
                     <p class="text-base font-bold">subheading</p>
@@ -23,7 +41,7 @@ export default BlogPosts;
 
 //Blog post template
 
-//     <div class="max-w-sm rounded overflow-hidden shadow-lg bg-slate-400">
+//     <div class="max-w-sm rounded overflow-hidden shadow-lg bg-slate-400 max-w-screen-lg">
 //         <h3 class="text-2xl font-bold font-mono text-center">Title</h3>
 //         <img src={require('../../Images/cameraPicture.jpg')} alt='me with a camera'></img>
 //         <p class="text-base font-bold">subheading</p>
