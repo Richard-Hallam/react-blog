2968e0355952afe1e535d4d332131211457fa264
Richard Hallam <62851758+Richard-Hallam@users.noreply.github.com>
2023-12-06 20:02:42 +0000

    tweaks to about and removed some boilerplate

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,12 +3,14 @@ import '../styles/About.css'
 
 function About(){
     return(
-        <div>
+        <div class="place-self-center">
           <div>
-            <p class= "text-base">I am a junior developer specialising in javascript, react, python and more. </p>
-            <p >Here are my github contributions</p>
-            <img class="items-center" src="http://ghchart.rshah.org/Richard-Hallam" alt="Richard-Hallam's Github chart" />
-            <p class="text-xs font-light items-center">The above image was generated using the following api https://github.com/2016rshah/githubchart-api</p>
+            <p class= "text-base text-center">I am a junior developer specialising in javascript, SQL, react, python and more. </p>
+            <p class=" text-base text-center">Here are my github contributions</p>
+            <p class="flex justify-center">
+                <img class="items-center" src="http://ghchart.rshah.org/Richard-Hallam" alt="Richard-Hallam's Github chart" />
+            </p>
+            <p class="text-xs font-light text-center">The above image was generated using the following api https://github.com/2016rshah/githubchart-api</p>
           </div>
           <div>
             <p class="text-lg font bold">Contact</p>
