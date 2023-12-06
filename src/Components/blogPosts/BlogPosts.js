63534cde2c55955328306ee94236339fa9e260b9
Richard Hallam <62851758+Richard-Hallam@users.noreply.github.com>
2023-12-06 18:58:33 +0000

    Made blog post template

--- src/Components/blogPosts/BlogPosts.js
+++ src/Components/blogPosts/BlogPosts.js
@@ -5,28 +5,13 @@ import '../../styles/BlogPosts.css';
 function BlogPosts(){
 
         return <div>
-            <ul className="post-list">
-              <li className="post">
-                <div>
-                    <h3>Hydroponics control system build 1</h3>
-                    <p className="blog-paragraph">THis is just some sample text that is not important right now Hopefully I come up with a better way to do this later on</p>
-                    <h3 className="subheading">bleh</h3>
-                    <p className="blog-paragraph">blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah 
-                      \nblah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah 
-                      /nblah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
-                       blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah 
-                      \nblah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah 
-                      /nblah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah 
-                      blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah 
-                      \nblah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah 
-                      /nblah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah 
-                      blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah 
-                      \nblah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah 
-                      /nblah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah 
-                      blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ss
-                      \nblah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah 
-                      /nblah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah </p>
+            <ul class="p-10">
+              <li class="flex ">
+                <div class="max-w-sm rounded overflow-hidden shadow-lg bg-slate-400">
+                    <h3 class="text-2xl font-bold font-mono text-center">Title</h3>
                     <img src={require('../../Images/cameraPicture.jpg')} alt='me with a camera'></img>
+                    <p class="text-base font-bold">subheading</p>
+                    <p class="text-base font-mono">body text</p>
                 </div>
               </li>
             </ul>
@@ -35,3 +20,12 @@ function BlogPosts(){
 
 
 export default BlogPosts;
+
+//Blog post template
+
+//     <div class="max-w-sm rounded overflow-hidden shadow-lg bg-slate-400">
+//         <h3 class="text-2xl font-bold font-mono text-center">Title</h3>
+//         <img src={require('../../Images/cameraPicture.jpg')} alt='me with a camera'></img>
+//         <p class="text-base font-bold">subheading</p>
+//         <p class="text-base font-mono">body text</p>
+//     </div>
