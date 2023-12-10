be1fb7a15c73a842453cbeb583e275298ec9fb89
Richard Hallam <62851758+Richard-Hallam@users.noreply.github.com>
2023-12-10 12:43:10 +0000

    fixed clickhandler

--- src/Components/mainPage/NavBar.js
+++ src/Components/mainPage/NavBar.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import '../../styles/NavBar.css';
 
 function Navbar(props) {
+    const [selectedButton, setSelectedButton] = useState('home')
+
+    function clickHandler(buttonName){
+        props.updatePageSetter(buttonName);
+        setSelectedButton(buttonName);
+    }
+
     return (
         <div className="navbar">
             <h2 className="nav-header">Richard Hallam</h2>
             <ul className="link-bar">
-                <button onClick={() => props.updatePageSetter('about')} class="bg-green-700 py-2 px-4 rounded-2xl">About</button>
-                <button onClick={() => props.updatePageSetter('projects')} class="bg-green-700 py-2 px-4 rounded-2xl">Projects</button>
-                <button onClick={() => props.updatePageSetter('posts')} class="bg-green-700 py-2 px-4 rounded-2xl">posts</button>
+                <button onClick={() => clickHandler('about')} class="bg-green-700 py-2 px-4 rounded-2xl">About</button>
+                <button onClick={() => clickHandler('projects')} class="bg-green-700 py-2 px-4 rounded-2xl">Projects</button>
+                <button onClick={() => clickHandler('posts')} class="bg-green-700 py-2 px-4 rounded-2xl">posts</button>
                 <a href="https://github.com/Richard-Hallam"><button class="bg-green-700 py-2 px-4 rounded-2xl">Github</button></a>
                 <a href="https://www.linkedin.com/in/richard-hallam-78391726b/"><button class="bg-green-700 py-2 px-4 rounded-2xl">Linkedin</button></a>
             </ul>
