a7730189bf4f25cdf3443e5e54f99aa855409f26
Richard Hallam <62851758+Richard-Hallam@users.noreply.github.com>
2023-12-12 18:10:34 +0000

    added js destructuring function for blog posts.

--- src/Components/blogPosts/postGenerator.js
+++ src/Components/blogPosts/postGenerator.js
@@ -1,5 +1,6 @@
-
-
 function importPost(postObj){
     const [title, heading, paragraph1, paragraph2, paragraph3, paragraph4, image1, image1AltText, image2, image2AltText] = postObj;
+    return postObj;
 }
+
+export default importPost;
