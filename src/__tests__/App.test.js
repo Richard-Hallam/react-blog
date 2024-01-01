de00187c4078ea1f802ce4d21f5533dc08c44ea8
Richard Hallam <62851758+Richard-Hallam@users.noreply.github.com>
2024-01-01 16:16:33 +0000

    updated old test in App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Richard Hallam/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('App', () => {
+  test('renders App component', () => {
+    render(<App/>)
+  })
+})
